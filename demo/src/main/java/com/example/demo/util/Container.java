28c335ca096808dda625664e5d8cef31bbd4aafa
zhuhtss <zht_xixi@163.com>
2019-12-04 23:01:22 +0800

    添加注释

--- demo/src/main/java/com/example/demo/util/Container.java
+++ demo/src/main/java/com/example/demo/util/Container.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 多线程容器
+ */
 public class Container {
     volatile List<Object> list = new ArrayList<>(10);
 
